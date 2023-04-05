7eeb2df5b8e1d4040813c62ce27c725eb2040f22
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-05 20:50:50 -0300

    :lipstick: Criando tema de cores   :lipstick:

---
+++ src/theme/index.ts
@@ -0,0 +1,13 @@
+const THEME = {
+  COLORS: {
+    PRIMARY: '#7159C1',
+    PRIMARY_TRANSLUCENT: '#7159C133',
+
+    DELETE: '#EF4444',
+
+    BLACK: '#000000',
+    WHITE: '#FFFFFF',
+  },
+};
+
+export default THEME;
