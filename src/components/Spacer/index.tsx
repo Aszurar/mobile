71b0d41401311fb1ca7ade8972fb109821805e44
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-05 20:14:04 -0300

    :sparkles: Criando componente Espacador :sparkles:

---
+++ src/components/Spacer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+type SpacerProps = {
+  vertical?: number;
+  horizontal?: number;
+};
+
+export function Spacer({ vertical = 0, horizontal = 0 }: SpacerProps) {
+  return <View style={{ width: horizontal, height: vertical }} />;
+}
