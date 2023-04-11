e9ec7ebd610c61b70602852b7db6d38d6f860dc2
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-10 21:07:17 -0300

    :sparkles: Adaptando input para  ser um textArea quando necessário, par amostrar as mensagens de erro e para usar a ref quando necessário   :sparkles:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { RefObject } from 'react';
 
 import {
   StyleSheet,
@@ -9,30 +9,80 @@ import {
 } from 'react-native';
 import THEME from '../../theme';
 
+const CHARACTERS_LIMIT = 150;
+
 type InputProps = TextInputProps & {
   label: string;
+  isOptional?: boolean;
+  height?: number;
+  limit?: number;
+  errorMessage?: string;
+  inputRef?: RefObject<TextInput>;
 };
-export function Input({ label, ...rest }: InputProps) {
+export function Input({ label, height, value, multiline, inputRef, isOptional = false, errorMessage, ...rest }: InputProps) {
+  const inputHeight = height || 48;
+  const isInvalid = errorMessage && !value;
+  const currentCharacters = value ? value.length : 0;
+
+  const borderBottomColor = isInvalid ? THEME.COLORS.DELETE : THEME.COLORS.PRIMARY;
+
+  const inputHeightStyle = { height: inputHeight }
+  const borderBottomColorStyle = { borderBottomColor }
+
+
+
+
   return (
     <View>
-      <Text style={styles.label}>{label}</Text>
-      <TextInput {...rest} style={styles.input} />
+      <View style={styles.textContainer}>
+        <Text style={styles.label}>{label}{!isOptional && <Text style={styles.labelObrigatory}>*</Text>}</Text>
+        {
+          currentCharacters > 0 && multiline &&
+          (
+            <Text style={[styles.label]}><Text style={styles.currentCharactersLimit}>{currentCharacters}</Text>/<Text style={styles.charactersLimit}>{CHARACTERS_LIMIT}</Text></Text>
+          )
+        }
+      </View>
+      <TextInput ref={inputRef} value={value} {...rest} style={[styles.input, inputHeightStyle, borderBottomColorStyle]} />
+      {
+        isInvalid &&
+        (
+          <Text style={styles.errorMessage}>{errorMessage}</Text>
+        )
+      }
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  textContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
   label: {
     fontSize: 12,
   },
+  charactersLimit: {
+    fontWeight: 'bold',
+  },
+  currentCharactersLimit: {
+    color: THEME.COLORS.PRIMARY,
+  },
+  labelObrigatory: {
+    fontWeight: 'bold',
+    color: THEME.COLORS.PRIMARY,
+  },
   input: {
-    height: 48,
     width: '100%',
     paddingHorizontal: 16,
     fontSize: 14,
     color: THEME.COLORS.BLACK,
     borderRadius: 12,
     borderBottomWidth: 1,
-    borderBottomColor: THEME.COLORS.PRIMARY,
   },
+  errorMessage: {
+    fontSize: 12,
+    color: THEME.COLORS.DELETE,
+  }
 });
