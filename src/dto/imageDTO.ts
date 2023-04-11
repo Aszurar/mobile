4d13b34196c7c68a9a0fe7fc33539609aba6092e
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-10 21:04:54 -0300

    :label: Adicionando DTO para auxiliar os componentes relacionados ao input de imagem :label:

---
+++ src/dto/imageDTO.ts
@@ -0,0 +1,34 @@
+import { Asset } from 'react-native-image-picker';
+
+export type ProjectImageProps = Pick<
+  Asset,
+  'width' | 'height' | 'fileName' | "fileSize" | 'type' | 'uri' | 'base64'
+>;
+
+const PROJECT_IMAGE_DEFAULT: ProjectImageProps = {
+  uri: '',
+  type: '',
+  width: 0,
+  height: 0,
+  fileName: '',
+  fileSize: 0,
+  base64: '',
+}
+
+const IMAGE_FILEZISE_LIMIT = 1000000;
+const IMAGE_MINIATURE_SIZE = 90;
+const IMAGE_BOX_MINIATURE_SIZE = 112;
+const IMAGE_QUANTITY_LIMIT_BY_REGISTER = 1;
+const IMAGE_RESOLUTION = {
+  height: 720,
+  width: 1366,
+};
+
+export {
+  IMAGE_RESOLUTION,
+  IMAGE_FILEZISE_LIMIT,
+  IMAGE_MINIATURE_SIZE,
+  PROJECT_IMAGE_DEFAULT,
+  IMAGE_BOX_MINIATURE_SIZE,
+  IMAGE_QUANTITY_LIMIT_BY_REGISTER,
+};
