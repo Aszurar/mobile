fbf2b35276f16264acde92495909998eb306a353
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-22 00:30:51 -0300

    :art: :recycle: formatando código e adicionando a prop fileSize no objeto de imagens :recycle::art:

--- src/components/ImagePickerButton/utils.ts
+++ src/components/ImagePickerButton/utils.ts
@@ -1,4 +1,5 @@
 import { Asset } from 'react-native-image-picker';
+
 import { ProjectImageProps } from '../../dto/imageDTO';
 
 //fizeSize é o tamanho máximo que a imagem pode ter em bytes
@@ -10,7 +11,7 @@ import { ProjectImageProps } from '../../dto/imageDTO';
  * @returns - Objeto com a imagem formatada.
  */
 function formatImageToState(image: Asset) {
-  const { uri, height, width, type, fileName,fileSize, base64 } = image;
+  const { uri, height, width, type, fileName, fileSize, base64 } = image;
 
   return {
     uri,
@@ -19,7 +20,7 @@ function formatImageToState(image: Asset) {
     height,
     fileName,
     base64,
-    fileSize
+    fileSize,
   } as ProjectImageProps;
 }
 
