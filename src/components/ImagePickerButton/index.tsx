614dbd28a0c7fdf1a6161d1414d0a1f13fce9237
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-04-22 00:32:02 -0300

    :art: :recycle: Formatando código e adicionando Alertas indicando o sucesso ou não da seleção de imagens :recycle: :art:

--- src/components/ImagePickerButton/index.tsx
+++ src/components/ImagePickerButton/index.tsx
@@ -1,30 +1,45 @@
 import React from 'react';
 
-import { ImageLibraryOptions, launchImageLibrary, } from 'react-native-image-picker';
 import {
-  Alert, Linking, Permission, PermissionsAndroid, Platform
-} from 'react-native';
-import { check, PERMISSIONS, request, PermissionStatus, RESULTS } from 'react-native-permissions';
+  check,
+  PERMISSIONS,
+  PermissionStatus,
+  request,
+  RESULTS,
+} from 'react-native-permissions';
+import {
+  ImageLibraryOptions,
+  launchImageLibrary,
+} from 'react-native-image-picker';
+import { Alert, Linking, Platform } from 'react-native';
 
 import ImageFileSVG from '../../assets/icons/file-image.svg';
 
 import {
-  ProjectImageProps, IMAGE_QUANTITY_LIMIT_BY_REGISTER, IMAGE_RESOLUTION, IMAGE_FILEZISE_LIMIT,
+  IMAGE_FILE_SIZE_LIMIT,
+  IMAGE_QUANTITY_LIMIT_BY_REGISTER,
+  IMAGE_RESOLUTION,
+  ProjectImageProps,
 } from '../../dto/imageDTO';
 
-import { formatImageToState } from './utils';
 import { IconNativeButton } from '../IconNativeButton';
+import { formatImageToState } from './utils';
 
 interface ImagePickerButtonProps {
   setProjectImage: (images: ProjectImageProps) => void;
-  // setImagesSelectedError: (errorMessage: string) => void;
+  setImagesSelectedError: (errorMessage: string) => void;
 }
 
 export function ImagePickerButton({
   setProjectImage,
-  // setImagesSelectedError,
+  setImagesSelectedError,
 }: ImagePickerButtonProps) {
-
+  /**
+   *Solicita permissão ao usuário para acessar a galeria de imagens do dispositivo.
+   *@returns {Promise<boolean>} Retorna uma Promise que resolve em true se a
+   *permissão foi concedida e false se foi negada.
+   *Em caso de erro, a função exibe um alerta de erro e retorna false.
+   */
   async function handleGalleryPermissionsRequest(): Promise<boolean> {
     try {
       let permissionStatus: PermissionStatus;
@@ -34,9 +49,13 @@ export function ImagePickerButton({
           permissionStatus = await request(PERMISSIONS.IOS.PHOTO_LIBRARY);
         }
       } else {
-        permissionStatus = await check(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+        permissionStatus = await check(
+          PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+        );
         if (permissionStatus === 'denied') {
-          permissionStatus = await request(PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE);
+          permissionStatus = await request(
+            PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE,
+          );
         }
       }
 
@@ -51,7 +70,10 @@ export function ImagePickerButton({
               text: 'Cancelar',
               style: 'cancel',
             },
-            { text: 'Ir para configurações', onPress: () => Linking.openSettings() },
+            {
+              text: 'Ir para configurações',
+              onPress: () => Linking.openSettings(),
+            },
           ],
         );
         return false;
@@ -65,7 +87,10 @@ export function ImagePickerButton({
             text: 'Cancelar',
             style: 'cancel',
           },
-          { text: 'Ir para configurações', onPress: () => Linking.openSettings() },
+          {
+            text: 'Ir para configurações',
+            onPress: () => Linking.openSettings(),
+          },
         ],
       );
       return false;
@@ -97,23 +122,34 @@ export function ImagePickerButton({
 
       if (response.assets) {
         if (response.assets.length > IMAGE_QUANTITY_LIMIT_BY_REGISTER) {
-          Alert.alert("Limite de imagens excedido", `Você pode selecionar apenas ${IMAGE_QUANTITY_LIMIT_BY_REGISTER} imagem. 
-          Apenas a primeira imagem será adicionada.`);
+          Alert.alert(
+            'Limite de imagens excedido',
+            `Você pode selecionar apenas ${IMAGE_QUANTITY_LIMIT_BY_REGISTER} 
+            imagem. 
+          Apenas a primeira imagem será adicionada.`,
+          );
         }
         const formattedImages = formatImageToState(response.assets[0]);
 
-        if (formattedImages.fileSize! > IMAGE_FILEZISE_LIMIT) {
+        if (formattedImages.fileSize! > IMAGE_FILE_SIZE_LIMIT) {
           console.log('========Tamanho da imagem excedido========');
           console.log(formattedImages.fileSize);
-          Alert.alert("Tamanho da imagem excedido", `O tamanho da imagem selecionada ultrapassa o máximo permitido de ${IMAGE_FILEZISE_LIMIT / IMAGE_FILEZISE_LIMIT}mb. Por favor, escolha uma imagem mais leve.`);
+          Alert.alert(
+            'Tamanho da imagem excedido',
+            `O tamanho da imagem selecionada ultrapassa o máximo permitido de 
+            ${IMAGE_FILE_SIZE_LIMIT} mb. Por favor, escolha uma imagem mais leve.`,
+          );
           return;
         }
 
         setProjectImage(formattedImages);
-        // setImagesSelectedError('');
+        setImagesSelectedError('');
       }
     } catch (error) {
-      Alert.alert("Erro ao selecionar imagens", "Ocorreu um erro ao selecionar imagens. Por favor, tente novamente.");
+      Alert.alert(
+        'Erro ao selecionar imagens',
+        'Ocorreu um erro ao selecionar imagens. Por favor, tente novamente.',
+      );
     }
   }
 
